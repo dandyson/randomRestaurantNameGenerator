7a1d670b99d9bb01673b680b8062dd4b2a65a29c
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-01-09 10:17:03 +0000

    Change to Restaurant Name Generator

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 const subBut = document.getElementById("submit");
 let resetBut = document.getElementById("reset");
 let input1 = document.getElementById("no1");
-let input2 = document.getElementById("no2");
+//let input2 = document.getElementById("no2");
 let answer = document.getElementById("newText");
+let verbArr = ["Running", "Shouting", "Laughing", "Playing", "Humping", "Jogging", "Inconsiderate", "Demeaning", "Menacing", "All-embracing"];
+let placesArr = ["Shack", "Grillhouse", "Deli", "Shed", "Cafe", "Steakhouse", "Brewery", "Watering-hole", "Brasserie", "Shop", "Saloon", "Bar"]
 
 
 subBut.addEventListener("click", () => {
-  answer.innerHTML = input1.value + " " + input2.value;
+  if (input1.value === "") {
+    answer.textContent = "Please enter your name!";
+  } else {
+  answer.textContent = `${input1.value}'s ${verbArr[Math.floor(Math.random() * verbArr.length)]} ${placesArr[Math.floor(Math.random() * placesArr.length)]}`;
+  }
 });
 
 resetBut.addEventListener("click", () => {
   input1.value = "";
-  input2.value = "";
   answer.textContent = "";
 });
